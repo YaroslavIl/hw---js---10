a90af6b202ce4864ee2d864ab9304019c9a19009
YaroslavIl <yaroslavovich90@gmail.com>
2023-01-25 09:55:40 +0200

    p1

--- 02/js/script.js
+++ 02/js/script.js
@@ -1,42 +1,61 @@
 
 
 let form = document.querySelector('form');
-let [...inputs] = form.elements;
 let btn = document.querySelector('.button');
 let sum = document.querySelector('.sum');
-let num = Number(sum);
-console.log(sum);
 let select = document.querySelectorAll('.select');
-console.log(typeof sum);
 let arr = Array.from(select);
-console.log(arr);
-let b = 0;
+
+let inedxArr = 0;
+let input = document.querySelectorAll('input');
+let inputArr = Array.from(input);
+let inputSum = 0;
+
+// ---------випадашки
 for (let i = 0; i < arr.length; i++){
-    b = arr[i];
-    b.addEventListener('change', function (e) {
+    inedxArr = arr[i];
+    // console.log(inedxArr);
+    inedxArr.addEventListener('change', function (e) {
         if (e.target.value > 0) {
-            sum.innerHTML = sum.innerHTML + e.target.value;
+            sum.innerHTML = +sum.innerHTML + +e.target.value;
+            console.log(e.target.value);
         }
-        // else {
-        //     sum.innerHTML = +sum.innerHTML - e.target.value;
-        // }
+        
+        if (arr[0].value > 0 && arr[1].value > 0 && arr[2].value > 0) {
+            btn.disabled = false;
+        }
+        if (arr[0].value == 0 || arr[1].value == 0 || arr[2].value == 0) {
+            btn.disabled = true;
+        }
     });
+  
+    
 }
+//  ------------- чекбокси
+function p() {
+    if (this.checked) {
+        sum.innerHTML = +sum.innerHTML + 10;
+    }
+    else {
+        sum.innerHTML = +sum.innerHTML - 10;
+    };
+};
 
+for (let j = 0; j < inputArr.length; j++) {
+    inputSum = inputArr[j];
+    inputSum.addEventListener('change', p);
+}
+// --------------- відправка форми + модальне вікно
+const elemModal = document.querySelector("#modal");
+const modal = new bootstrap.Modal(elemModal);
 
+function sec() {
 
-// btn.onclick = function (e) {
-//     e.preventDefault();
+    modal.hide();
+};
 
-//     for (let i = 0; i < inputs.length; i++) {
-//         let a = inputs[i];
-//         console.log(a.type);
-//         if (a.type == 'checkbox') {
-//             if (a.checked) {
-//                 console.log(typeof a.value);
-//                 let num = a.value
-//                 sum.innerText = +sum.innerText + num;
-//             }
-//         }
-//     }
-// }
+btn.onclick = function (e) {
+    e.preventDefault()
+    setInterval(sec, 5000);
+
+};
