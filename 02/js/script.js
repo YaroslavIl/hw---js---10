5d5a6fc5ea6aea70b0af9a49b76302c95f24d6ab
YaroslavIl <yaroslavovich90@gmail.com>
2023-02-02 18:57:10 +0200

    fix#1

--- 02/js/script.js
+++ 02/js/script.js
@@ -6,21 +6,59 @@ let sum = document.querySelector('.sum');
 let select = document.querySelectorAll('.select');
 let arr = Array.from(select);
 
-let inedxArr = 0;
 let input = document.querySelectorAll('input');
 let inputArr = Array.from(input);
 let inputSum = 0;
+let arr1 = 0;
+let arr2 = 0;
+let arr3 = 0;
+
 
 // ---------випадашки
-for (let i = 0; i < arr.length; i++){
-    inedxArr = arr[i];
-    // console.log(inedxArr);
-    inedxArr.addEventListener('change', function (e) {
-        if (e.target.value > 0) {
-            sum.innerHTML = +sum.innerHTML + +e.target.value;
-            console.log(e.target.value);
+
+form.addEventListener('change', function (e) {
+    for (let i = 0; i < arr.length; i++) {
+    }
+
+        if (arr[0]) {
+
+            if (arr[0].value > 0) {
+                sum.innerHTML = +sum.innerHTML + +arr[0].value;
+                sum.innerHTML = +sum.innerHTML - arr1;
+            }
+            if (arr[0].value == 0) {
+                sum.innerHTML = +sum.innerHTML - arr1;
+                 arr1 = 0;;
+            }
+            arr1 = arr[0].value;
         }
-        
+
+        if (arr[1]) {
+            sum.innerHTML = +sum.innerHTML + +arr[1].value;
+
+            if (arr[1].value > 0) {
+                sum.innerHTML = +sum.innerHTML - arr2;
+            }
+            if (arr[1].value == 0) {
+                sum.innerHTML = +sum.innerHTML - arr2;
+                arr2 = 0;
+            }
+            arr2 = arr[1].value;
+        }
+
+        if (arr[2]) {
+            sum.innerHTML = +sum.innerHTML + +arr[2].value;
+
+            if (arr[2].value > 0) {
+                sum.innerHTML = +sum.innerHTML - arr3;
+            }
+            if (arr[2].value == 0) {
+                sum.innerHTML = +sum.innerHTML - arr3;
+                 arr3 = 0;;
+            }
+            arr3 = arr[2].value;
+        }
+
         if (arr[0].value > 0 && arr[1].value > 0 && arr[2].value > 0) {
             btn.disabled = false;
         }
@@ -28,9 +66,10 @@ for (let i = 0; i < arr.length; i++){
             btn.disabled = true;
         }
     });
-  
-    
-}
+
+
+
+
 //  ------------- чекбокси
 function p() {
     if (this.checked) {
